550d53860fbe49af1ca9c08f2ccbd73b1ee239fa
Nagesh Podilapu <nagesh.podilapu@gmail.com>
2024-10-05 12:47:47 +0200

    Configure Mac OS settings with nix-darwin

--- darwin.nix
+++ darwin.nix
@@ -35,6 +35,7 @@
     git
     tig
     imgcat
+    nixpkgs-fmt
   ];
 
   environment.variables = {
@@ -46,11 +47,12 @@
 
   homebrew = {
     enable = true;
-    taps = [];
-    brews = [];
+    taps = [ ];
+    brews = [ ];
     casks = [
       "1password"
       "1password-cli"
+      "iterm2"
     ];
 
     onActivation = {
@@ -63,5 +65,83 @@
   # Enable Touch ID auth for sudo
   security.pam.enableSudoTouchIdAuth = true;
 
-  system.defaults.dock.autohide = true;
+  system.defaults.dock = {
+    autohide = true;
+    autohide-delay = 0.0;
+    tilesize = 48;
+    launchanim = false;
+    static-only = false;
+    showhidden = true;
+    show-recents = false;
+    show-process-indicators = true;
+    mru-spaces = false;
+  };
+
+  system.defaults.finder = {
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    FXDefaultSearchScope = "SCcf"; # Seach only current folder
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "icnv";
+    QuitMenuItem = true;
+    ShowStatusBar = true;
+    _FXSortFoldersFirst = true;
+  };
+
+  system.defaults.NSGlobalDomain = {
+    AppleInterfaceStyle = "Dark";
+    AppleShowScrollBars = "Automatic";
+    "com.apple.keyboard.fnState" = true;
+  };
+
+  system.defaults.trackpad.Clicking = true;
+
+  system.defaults.CustomUserPreferences = {
+    NSGlobalDomain = {
+      # Add a context menu item for showing the Web Inspector in web views
+      WebKitDeveloperExtras = true;
+    };
+    "com.apple.finder" = {
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowMountedServersOnDesktop = true;
+      ShowRemovableMediaOnDesktop = true;
+    };
+    "com.apple.desktopservices" = {
+      # Avoid creating .DS_Store files on network or USB volumes
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
+    "com.apple.screencapture" = {
+      location = "~/Desktop";
+      type = "jpg";
+    };
+    "com.apple.AdLib" = {
+      allowApplePersonalizedAdvertising = false;
+    };
+    "com.apple.print.PrintingPrefs" = {
+      # Automatically quit printer app once the print jobs complete
+      "Quit When Finished" = true;
+    };
+    "com.apple.SoftwareUpdate" = {
+      AutomaticCheckEnabled = true;
+      # Check for software updates daily, not just once per week
+      ScheduleFrequency = 1;
+      # Download newly available updates in background
+      AutomaticDownload = 1;
+      # Install System data files & security updates
+      CriticalUpdateInstall = 1;
+    };
+    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+    # Prevent Photos from opening automatically when devices are plugged in
+    "com.apple.ImageCapture".disableHotPlug = true;
+    # Turn on app auto-update
+    "com.apple.commerce".AutoUpdate = true;
+  };
+
+  system.activationScripts.postUserActivation.text = ''
+    # activateSettings -u will reload the settings from the database and
+    # apply them to the current session, so we do not need to logout and
+    # login again to make the changes take effect.
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
 }
