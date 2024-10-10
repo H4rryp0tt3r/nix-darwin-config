ded37ac447c9383890b099e1128e066f0e1d7b13
Nagesh Podilapu <nagesh.podilapu@gmail.com>
2024-10-11 00:41:01 +0200

    Arrange the system config

--- darwin.nix
+++ darwin.nix
@@ -82,123 +82,123 @@
   # Enable Touch ID auth for sudo
   security.pam.enableSudoTouchIdAuth = true;
 
-  system.defaults.dock = {
-    autohide = true;
-    autohide-delay = 0.0;
-    tilesize = 48;
-    launchanim = false;
-    static-only = false;
-    showhidden = true;
-    show-recents = false;
-    show-process-indicators = true;
-    mru-spaces = false;
-    persistent-apps = [
-      "/Applications/Firefox.app"
-      "/Applications/iTerm.app"
-      "/Users/h4rryp0tt3r/Applications/Home Manager Apps/Visual Studio Code.app" # TODO: Pass username from the calling site
-    ];
-    # Disable hot corners
-    wvous-bl-corner = 1;
-    wvous-br-corner = 1;
-    wvous-tl-corner = 1;
-    wvous-tr-corner = 1;
-  };
-
-  system.defaults.finder = {
-    AppleShowAllExtensions = true;
-    AppleShowAllFiles = true;
-    FXDefaultSearchScope = "SCcf"; # Seach only current folder
-    FXEnableExtensionChangeWarning = false;
-    FXPreferredViewStyle = "icnv";
-    QuitMenuItem = true;
-    ShowStatusBar = true;
-    _FXSortFoldersFirst = true;
-  };
-
-  system.defaults.NSGlobalDomain = {
-    AppleInterfaceStyle = "Dark";
-    AppleShowScrollBars = "Automatic";
-    "com.apple.keyboard.fnState" = true;
-  };
-
-  system.defaults.loginwindow = {
-    GuestEnabled = false;
-    SHOWFULLNAME = true;
-  };
-
-  system.defaults.trackpad.Clicking = true;
-
-  system.defaults.screencapture = {
-    location = "~/Desktop";
-    type = "jpg";
-    disable-shadow = true;
-  };
-
-  system.defaults.CustomUserPreferences = {
-    "com.apple.touchbar.agent" = {
-      "presentationModeGlobal" = "functionKeys";
+  system.defaults = {
+    dock = {
+      autohide = true;
+      autohide-delay = 0.0;
+      tilesize = 48;
+      launchanim = false;
+      static-only = false;
+      showhidden = true;
+      show-recents = false;
+      show-process-indicators = true;
+      mru-spaces = false;
+      persistent-apps = [
+        "/Applications/Firefox.app"
+        "/Applications/iTerm.app"
+        "/Users/h4rryp0tt3r/Applications/Home Manager Apps/Visual Studio Code.app" # TODO: Pass username from the calling site
+      ];
+      # Disable hot corners
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
     };
-    "com.apple.symbolichotkeys" = {
-      AppleSymbolicHotKeys."64".enabled = 0; # Disable Spotlight Search shortcut (Cmd + Space)
+
+    finder = {
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      FXDefaultSearchScope = "SCcf"; # Seach only current folder
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "icnv";
+      QuitMenuItem = true;
+      ShowStatusBar = true;
+      _FXSortFoldersFirst = true;
     };
-    "com.raycast.macos" = {
-      onboardingCompleted = 1;
-      raycastGlobalHotkey = "Command-49";
-      raycastShouldFollowSystemAppearance = 1;
-    };
-    "com.generalarcade.flycut" = {
-      # Set the number of items to keep in the clipboard history
-      rememberNum = 100;
-      loadOnStartup = 1;
-      wraparoundBezel = 1;
-      displayClippingSource = 1;
-      bezelAlpha = "0.8";
-      bezelHeight = "400";
-      bezelWidth = "600";
-      menuIcon = 2;
-      removeDuplicates = 1;
-    };
+
     NSGlobalDomain = {
-      # Add a context menu item for showing the Web Inspector in web views
-      WebKitDeveloperExtras = true;
+      AppleInterfaceStyle = "Dark";
+      AppleShowScrollBars = "Automatic";
+      "com.apple.keyboard.fnState" = true;
     };
-    "com.apple.finder" = {
-      ShowExternalHardDrivesOnDesktop = true;
-      ShowMountedServersOnDesktop = true;
-      ShowRemovableMediaOnDesktop = true;
+
+    loginwindow = {
+      GuestEnabled = false;
+      SHOWFULLNAME = true;
     };
-    "com.apple.desktopservices" = {
-      # Avoid creating .DS_Store files on network or USB volumes
-      DSDontWriteNetworkStores = true;
-      DSDontWriteUSBStores = true;
+
+    trackpad.Clicking = true;
+
+    screencapture = {
+      location = "~/Desktop";
+      type = "jpg";
+      disable-shadow = true;
     };
-    "com.apple.AdLib" = {
-      allowApplePersonalizedAdvertising = false;
+
+    CustomUserPreferences = {
+      NSGlobalDomain = {
+        # Add a context menu item for showing the Web Inspector in web views
+        WebKitDeveloperExtras = true;
+      };
+      "com.apple.touchbar.agent" = {
+        "presentationModeGlobal" = "functionKeys";
+      };
+      "com.apple.symbolichotkeys" = {
+        AppleSymbolicHotKeys."64".enabled = 0; # Disable Spotlight Search shortcut (Cmd + Space)
+      };
+      "com.raycast.macos" = {
+        onboardingCompleted = 1;
+        raycastGlobalHotkey = "Command-49";
+        raycastShouldFollowSystemAppearance = 1;
+      };
+      "com.generalarcade.flycut" = {
+        # Set the number of items to keep in the clipboard history
+        rememberNum = 100;
+        loadOnStartup = 1;
+        wraparoundBezel = 1;
+        displayClippingSource = 1;
+        bezelAlpha = "0.8";
+        bezelHeight = "400";
+        bezelWidth = "600";
+        menuIcon = 2;
+        removeDuplicates = 1;
+      };
+      "com.apple.finder" = {
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowMountedServersOnDesktop = true;
+        ShowRemovableMediaOnDesktop = true;
+      };
+      "com.apple.desktopservices" = {
+        # Avoid creating .DS_Store files on network or USB volumes
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+      "com.apple.AdLib" = {
+        allowApplePersonalizedAdvertising = false;
+      };
+      "com.apple.print.PrintingPrefs" = {
+        # Automatically quit printer app once the print jobs complete
+        "Quit When Finished" = true;
+      };
+      "com.apple.SoftwareUpdate" = {
+        AutomaticCheckEnabled = true;
+        # Check for software updates daily, not just once per week
+        ScheduleFrequency = 1;
+        # Download newly available updates in background
+        AutomaticDownload = 1;
+        # Install System data files & security updates
+        CriticalUpdateInstall = 1;
+      };
+      "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+      # Prevent Photos from opening automatically when devices are plugged in
+      "com.apple.ImageCapture".disableHotPlug = true;
+      # Turn on app auto-update
+      "com.apple.commerce".AutoUpdate = true;
     };
-    "com.apple.print.PrintingPrefs" = {
-      # Automatically quit printer app once the print jobs complete
-      "Quit When Finished" = true;
-    };
-    "com.apple.SoftwareUpdate" = {
-      AutomaticCheckEnabled = true;
-      # Check for software updates daily, not just once per week
-      ScheduleFrequency = 1;
-      # Download newly available updates in background
-      AutomaticDownload = 1;
-      # Install System data files & security updates
-      CriticalUpdateInstall = 1;
-    };
-    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
-    # Prevent Photos from opening automatically when devices are plugged in
-    "com.apple.ImageCapture".disableHotPlug = true;
-    # Turn on app auto-update
-    "com.apple.commerce".AutoUpdate = true;
   };
 
   system.activationScripts.postUserActivation.text = ''
-    # activateSettings -u will reload the settings from the database and
-    # apply them to the current session, so we do not need to logout and
-    # login again to make the changes take effect.
+    # activateSettings -u will reload the settings without requiring logout
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
   '';
 }
